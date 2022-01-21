abddc536ba8a65ce0cf4ff9cc561fc075278ed39
Curtis Kettler <curtiskettler@Curtiss-MBP.lan>
2022-01-21 16:08:19 -0600

    submit add skill for working correctly

--- src/SkillForm.jsx
+++ src/SkillForm.jsx
@@ -43,7 +43,11 @@ export function SkillForm () {
 
     const handleSubmitButtonClick = (event) => {
         event.preventDefault();
-        console.log(addSkill);
+        dispatch({
+            type: 'ADD_SKILL',
+            payload: addSkill
+        });
+        navigate('/skilllist');
     };
 
 
