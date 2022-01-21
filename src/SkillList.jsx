02fe0737fbd3c8d1025515b9ac5792bdee3fbb55
Curtis Kettler <curtiskettler@Curtiss-MBP.lan>
2022-01-21 10:34:39 -0600

    work on filter bar and table for skill list page

--- src/SkillList.jsx
+++ src/SkillList.jsx
@@ -9,7 +9,11 @@ import {
   FilterGroupItem,
   Input,
   ComboBox,
-  ComboBoxItem 
+  ComboBoxItem, 
+  Title,
+  Button,
+  FlexBox,
+  CheckBox
 } from "@ui5/webcomponents-react";
 
 export function SkillList() {
@@ -20,15 +24,33 @@ export function SkillList() {
         <Input placeholder="Search" />
       </FilterGroupItem>
       <FilterGroupItem>
-        <ComboBox>
+        <ComboBox placeholder="Skill Type">
+          <ComboBoxItem text="ComboBoxItem 1" />
+          <ComboBoxItem text="ComboBoxItem 2" />
+          <ComboBoxItem text="ComboBoxItem 3" />
+        </ComboBox>
+      </FilterGroupItem>
+      <FilterGroupItem>
+        <ComboBox placeholder="Department">
           <ComboBoxItem text="ComboBoxItem 1" />
           <ComboBoxItem text="ComboBoxItem 2" />
           <ComboBoxItem text="ComboBoxItem 3" />
         </ComboBox>
       </FilterGroupItem>
     </FilterBar>
+    <FlexBox justifyContent="SpaceBetween" style={{ marginLeft: '20px', marginRight: '20px', marginTop: '20px'}}>
+      <Title>Skills</Title>
+      <FlexBox>
+        <Button design="Transparent">Create</Button>
+        <Button design="Transparent">Copy</Button>
+        <Button design="Transparent">Delete</Button>
+      </FlexBox>
+    </FlexBox>
     <Table style={{ margin: '20px'}}>
-      <TableColumn style={{ width: '20rem'}}>
+      <TableColumn style={{ width: '3rem'}}>
+        <CheckBox />
+      </TableColumn>
+      <TableColumn style={{ width: '25rem'}}>
         <Label>Skill</Label>
       </TableColumn>
       <TableColumn>
@@ -38,7 +60,32 @@ export function SkillList() {
         <Label>Department</Label>
       </TableColumn>
       <TableRow>
-
+        <TableCell>
+          <CheckBox />
+        </TableCell>
+        <TableCell>
+          <Label>Bachelor of Science</Label>
+        </TableCell>
+        <TableCell>
+          <Label>University Degree</Label>
+        </TableCell>
+        <TableCell>
+          <Label>n/a</Label>
+        </TableCell>
+      </TableRow>
+      <TableRow>
+        <TableCell>
+          <CheckBox />
+        </TableCell>
+        <TableCell>
+          <Label>SAP Fiori Level 1</Label>
+        </TableCell>
+        <TableCell>
+          <Label>SAP Certification</Label>
+        </TableCell>
+        <TableCell>
+          <Label>SAP</Label>
+        </TableCell>
       </TableRow>
     </Table>
     </>
